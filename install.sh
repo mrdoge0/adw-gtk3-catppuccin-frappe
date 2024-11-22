d59e2b2fa504342450d6d0308235f15e88dcb689
Mr. Doge <78793304+mrdoge0@users.noreply.github.com>
2024-11-22 22:20:48 +0300

    Add failsafes and make output better

--- install.sh
+++ install.sh
@@ -1,13 +1,28 @@
 #!/usr/bin/env bash
-# Install script.
+# Install script - for adw-gtk3-catppuccin-frappe
 
-# Check if root
-if [ "$EUID" -ne 0 ]
-  then echo "Please try again as root"
+echo "adw-gtk3-catppuccin-frappe - install script"
+
+# Check if root, and exit if not
+if [ "$EUID" -ne 0 ] then
+  echo "Please try again as root"
   exit 1
 fi
 
-# Copy the theme
+# Check if PWD is correct, and exit if not
+if [ -nd "./adw-gtk3-catppuccin-frappe" ] then
+  echo "PWD isn't correct. Try again with correct PWD."
+  exit 1
+fi
+
+# Check if theme is already installed, and delete it if yes
+if [ -d "/usr/share/themes/adw-gtk3-catppuccin-frappe" ] then
+  echo "Deleting old theme..."
+  rm -rf "/usr/share/themes/adw-gtk3-catppuccin-frappe"
+fi
+
+# Install new theme
+echo "Installing new theme..."
 cp -r ./adw-gtk3-catppuccin-frappe /usr/share/themes
 
 # Exit with exit code of cp command
